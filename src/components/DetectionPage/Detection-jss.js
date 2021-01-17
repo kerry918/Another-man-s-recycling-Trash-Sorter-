3ca6da07e9d5c957afdacbd7b520e501f5e13ab2
nadinelin <57516076+nadinelin@users.noreply.github.com>
2021-01-17 00:25:21 -0500

    I tried

--- src/components/DetectionPage/Detection-jss.js
+++ src/components/DetectionPage/Detection-jss.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-
+        background: "#DBDFDF", 
+        height: '100vh'
     }
 }));
 
